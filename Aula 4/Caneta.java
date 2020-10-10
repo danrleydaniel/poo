62f41fbfe066372f41f0bf55d1931be15a70efd0
danrleydaniel <danrleydaniel21@gmail.com>
2020-10-10 13:28:54 -0300

    Métodos setters, getters e construtores

---
+++ Aula 4/Caneta.java
@@ -0,0 +1,42 @@
+public class Caneta{
+  private String modelo;
+  private float ponta;
+  private boolean tampada;
+  private String cor;
+
+  public Caneta{
+    this.tampar();
+    this.cor = "Azul";
+  }
+
+  public String getModelo(){
+    return this.modelo;
+  }
+
+  public void setModelo(String m){
+    this.modelo = m;
+  }
+
+  public float getPonta(){
+    return this.ponta;
+  }
+
+  public void setPonta(float p){
+    this.ponta = p;
+  }
+
+  public void tampar(){
+    this.tampada = true;
+  }
+
+  public void destampar(){
+    this.tampada = false;
+  }
+
+  public void status(){
+    System.out.println("Modelo da caneta: " +  this.modelo);
+    System.out.println("Tamanho da ponta: " + this.ponta);
+    System.out.println("A cor da caneza é " +  this.cor);
+    System.out.println("A caneta está tampada? " +  this.tampada);
+  }
+}
