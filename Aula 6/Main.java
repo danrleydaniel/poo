5c555c7e19a04b31a0f7787411f0cf57ebbcbe4c
danrleydaniel <danrleydaniel21@gmail.com>
2020-10-20 13:57:12 -0300

    Encapsulamento

---
+++ Aula 6/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    ControleRemoto c = new ControleRemoto();
+    c.desligar();
+    c.maisVolume();
+    c.play();
+    c.abrirMenu();
+    c.fecharMenu();
+  }
+}
