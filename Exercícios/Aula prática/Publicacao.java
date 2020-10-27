387969dda3deb88c0e89f5dd4907c66f7958bbfa
danrleydaniel <danrleydaniel21@gmail.com>
2020-10-27 15:34:30 -0300

    Classes, objetos, relacionamentos e encapsulamento

---
+++ Exercícios/Aula prática/Publicacao.java
@@ -0,0 +1,10 @@
+public interface Publicacao{
+
+  public abstract void abrir();
+  public abstract void fechar();
+  public abstract void folhear(int n);
+  public abstract void avancarPag();
+  public abstract void voltarPag();
+  public abstract void reiniciar();
+  
+}
