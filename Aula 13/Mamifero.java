9f3cbf745e91c4e3b6377099d0545900e705f031
danrleydaniel <danrleydaniel21@gmail.com>
2020-11-12 13:05:58 -0300

    Polimorfismo - parte 2: Sobrecarga

---
+++ Aula 13/Mamifero.java
@@ -0,0 +1,10 @@
+public class Mamifero extends Animal{
+  //ATRIBUTOS
+  protected String corPelo;
+
+  //MÉTODOS
+  @Override
+  public void emitirSom(){
+    System.out.println("Som de mamífero");
+  }
+}
