fa6799ef53802bb32dc99559726859bb7e7726e9
danrleydaniel <danrleydaniel21@gmail.com>
2020-11-17 14:23:16 -0300

    Classe principal

---
+++ Exercícios/Aula prática 2/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    Video v[] = new Video[3];
+    v[0] = new Video("Aula 1 de POO");
+    v[1] = new Video("Aula 12 de PHP");
+    v[2] = new Video("Aula 10 de HTML");
+
+    Gafanhoto g[] = new Gafanhoto[2];
+    g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
+    g[1] = new Gafanhoto("Creuziane", 21, "F", "creuza");
+
+    v[0].mostrar();
+    v[1].mostrar();
+    v[2].mostrar();
+
+    System.out.println("\n");
+    g[0].mostrar();
+    g[1].mostrar();
+  }
+}
